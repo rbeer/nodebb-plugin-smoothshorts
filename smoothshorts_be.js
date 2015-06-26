86dfb68a0ebb4f3e221dff1bfe242b79f2a921d1
Raphael Beer <raphaelbeer030@gmail.com>
2015-06-26 10:22:46 +0200

    cleaning up db calls mess/async

--- smoothshorts_be.js
+++ smoothshorts_be.js
@@ -30,83 +30,79 @@ SmoothShorts.init = function(app, cb) {
   return cb(null, app);
 };
 
-SmoothShorts.resolveHash = function(req, res, next) {
+SmoothShorts.resolveHash = function(req, res, cb) {
   var hash = req.params.hash;
+  console.log(hash);
 
-  // async this mess!
-  // better yet, take the time to get familiar with db.*
-  // and sort it out!
-  // holy crap ^_^ - use db.sortedSetScore!
-  db.isSortedSetMember('posts:smoothshorts', hash, function(err, isPostHash) {
-    if (isPostHash) {
-      /* if this hash points to a post */
-      // get data row (rank) of hash/postid
-      db.sortedSetRank('posts:smoothshorts', hash, function(err, rank) {
-        // get postid from that row (rank)
-        db.getSortedSetRangeWithScores('posts:smoothshorts', rank, rank, function(err, range) {
-          // postid for hash
-          var pid = range[0].score;
-          // get topicid this pid belongs to
+  async.parallel({
+    isPost: function(next) {
+      db.isSortedSetMember('posts:smoothshorts', hash, next);
+    },
+    isTopic: function(next) {
+      db.isSortedSetMember('topics:smoothshorts', hash, next);
+    }
+  }, function(err, result) {
+    if (err) {
+      return cb(err);
+    } else if (result.isPost) {
+      async.waterfall([
+        function(next) {
+          db.sortedSetScore('posts:smoothshorts', hash, next);
+        },
+        function(pid, next) {
           db.getObjectField('post:' + pid, 'tid', function(err, tid) {
-            /* is this a non-initial post? (i.e. not the first post in topic) */
-            db.isSortedSetMember('tid:' + tid + ':posts', pid, function(err, isNotInitial) {
-              if (isNotInitial) {
-                /* yes, it is */
-                // get position in topic listing (i.e. data-index in DOM)
-                db.sortedSetRank('tid:' + tid + ':posts', pid, function(err, postInTopic) {
-                  // get topic slug; needed for URI
-                  db.getObjectField('topic:' + tid, 'slug', function(err, slug) {
-                    // build URI
-                    var uri = '/topic/' + slug + '/' + (parseInt(postInTopic, 10) + 2).toString();
-                    winston.verbose('[plugins:SmoothShorts] Redirecting ' +
-                                    hash + ' to post(' + pid + ') ' + uri);
-                    // redirect user to URI
-                    res.redirect(uri);
-                  });
-                });
-              } else {
-                /* no, it's not; simply redirect to topic */
-                // get topic slug (i.e. 'tid/title'); needed for URI
-                db.getObjectField('topic:' + tid, 'slug', function(err, slug) {
-                  // build URI
-                  var uri = '/topic/' + slug + '/1';
-                  winston.verbose('[plugins:SmoothShorts] Redirecting ' + hash +
-                                  ' to post(' + pid + ') ' + uri);
-                  // redirect user to URI
-                  res.redirect(uri);
-                }); // Did you ever
-              }     // wonder where
-            });     // the term
-          });       // callback hell
-        });         // comes from?
-      });           // Well, here you go. :]
-    } else {
-      // if this hash points to a topic
-      db.isSortedSetMember('topics:smoothshorts', hash, function(err, isTopicHash) {
-        if (isTopicHash) {
-          // get data row (rank) of hash/topicid
-          db.sortedSetRank('topics:smoothshorts', hash, function(err, rank) {
-            // get topicid from that row (rank)
-            db.getSortedSetRangeWithScores('topics:smoothshorts', rank, rank, function(err, range) {
-              var tid = range[0].score;
-              db.getObjectField('topic:' + tid, 'slug', function(err, slug) {
-                // build URI
-                var uri = '/topic/' + slug + '/';
-                winston.verbose('[plugins:SmoothShorts] Redirecting ' + hash +
-                                ' to topic(' + tid + ') ' + uri);
-                // redirect user to URI
-                res.redirect(uri);
-              });
-            });
+            return next(null, {pid: pid, tid: tid});
           });
-        } else {
-          // at this point it's clear that
-          // this is neither a post- nor topic-hash
-          // show some error message
-          winston.warn('[plugins:smoothshorts] Couldn\'t resolve ' + hash);
-          next(null);
+        },
+        function(data, next) {
+          async.parallel({
+            rank: function(next) {
+              db.sortedSetRank('tid:' + data.tid + ':posts', data.pid, next);
+            },
+            slug: function(next) {
+              db.getObjectField('topic:' + data.tid, 'slug', next);
+            }
+          }, function(err, result) {
+            if (err) {
+              return cb(err);
+            }
+            var pIndex = (result.rank !== null) ?
+                         (parseInt(result.rank, 10) + 2).toString() : '1';
+            var uri = '/topic/' + result.slug + '/' + pIndex;
+            next(null, uri);
+          });
         }
+      ], function(err, uri) {
+        if (err) {
+          return cb(err);
+        }
+        winston.verbose('[plugins:SmoothShorts] Redirecting ' + hash +
+                        ' to ' + uri);
+        res.redirect(uri);
       });
+    } else if (result.isTopic) {
+      /* if this hash points to a topic */
+      async.waterfall([
+        function(next) {
+          db.sortedSetScore('topics:smoothshorts', hash, next);
+        },
+        function(tid, next) {
+          db.getObjectField('topic:' + tid, 'slug', next);
+        }
+      ], function(err, slug) {
+        if (err) {
+          return cb(err);
+        }
+        // build URI
+        var uri = '/topic/' + slug + '/';
+        winston.verbose('[plugins:SmoothShorts] Redirecting ' + hash +
+                        ' to ' + uri);
+        // redirect user to URI
+        res.redirect(uri);
+      });
+    } else {
+      winston.warn('[plugins:SmoothShorts] Couldn\'t resolve ' + hash);
+      cb(null);
     }
   });
 };
