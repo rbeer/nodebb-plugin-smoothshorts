a3c8758e79ae1dcb0253974ba6941edc9a15cfd8
Raphael Beer <rbeerdev@gmail.com>
2016-04-10 15:09:23 +0200

    Fix missing parameter/message on app.AlertError

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -33,7 +33,7 @@
         var fn = helper[type + 'Map'];
         return fn(mapObj, type === 'topics' ? HashedTopic : HashedPost);
       };
-    };
+    }
 
     function addOnScrollLoad(event, data) {
       var type = event.type.split(':')[1];
@@ -57,7 +57,11 @@
         isCopied = document.execCommand('copy');
         msg = isCopied ? config.i18nStrings.hashbutton.success.replace('%1', url) :
                          config.i18nStrings.hashbutton.error;
-        isCopied ? app.alertSuccess(msg) : app.alertError;
+        if (isCopied) {
+          app.alertSuccess(msg);
+        } else {
+          app.alertError(msg);
+        }
       };
       return copyShortUrl;
     }
