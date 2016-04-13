34abeaf1cc8f20d4198fd4a421dd423ed4a7f698
Raphael Beer <rbeerdev@gmail.com>
2016-04-13 06:36:02 +0200

    Remove not needed 'fallback' message string

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -2,15 +2,10 @@
 
 define('plugins/smoothshorts/config', ['translator'], function(i18n) {
   var ssConfig = {
-    fallback: true,
     // modifier key (ctrl, alt or shift) to enable uri replacement
     modKey: '',
     // Short URLs will use this domain, if set
-    forcedDomain: '',
-    // support introduced in https://github.com/NodeBB/NodeBB/commit/31815f7d226243411468d6ef230a5f1968b47dcd
-    // tagged for NodeBB v1.0.3
-    // workaround for full compatibility in the making :)
-    noTeaserInfo: '[plugins:smoothshorts] This NodeBB version doesn\'t support teasers to be shortened, sorry :/\nYou can still visit the post itself and copy the short url from there! :D'
+    forcedDomain: ''
   };
 
   ssConfig.load = function(cb) {
